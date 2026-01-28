adef0b63886df13fba18c68326390e4b30072545
Michael Berrios <mxbics@rit.edu>
2026-01-28 11:03:17 -0500

    Exception handling and tryparse

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,23 @@
+﻿int number;
+
+while(true)
+{
+    Console.Write("Enter a number: ");
+
+    string input = Console.ReadLine();
+
+    try
+    {
+        number = int.Parse(input);
+
+        // if line 11 successfully parses, then break from the loop
+        break;
+    }
+    catch (FormatException exception)
+    {
+        Console.WriteLine($"Could not convert {input} to integer");
+        Console.WriteLine(exception.Message);
+    }    
+}
+
+Console.WriteLine($"The number is {number}");
---
+++ TryParse/Program.cs
@@ -0,0 +1,20 @@
+﻿while(true)
+{
+    Console.Write("Enter a number: ");
+
+    string input = Console.ReadLine();
+
+    // bool passed = int.TryParse(input, out int number)
+    // if(passed)
+    
+    if(int.TryParse(input, out int number))
+    {
+        Console.WriteLine($"The number is {number}");
+        
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Failed to convert input to an integer");
+    }
+}
